1742a6a900033ce7c1aabc29d652250d22c7960f
arjunreddy2293 <muthangiarjunreddy@gmail.com>
2025-08-04 09:10:58 +0530

    Add files via upload

---
+++ legacy.java
@@ -0,0 +1,21 @@
+package practical;
+import java.util.*;
+public class legacy {
+	
+	public static void main(String args[])
+	{
+	Date d=new Date();
+	System.out.println("Current date is" +d);
+	int month=d.getMonth();
+	System.out.println(month);
+	int year=d.getYear();
+	System.out.println(year);
+	System.out.println(d.getMinutes());
+	System.out.println(d.getHours());
+	}
+	}
+
+
+
+
+
